f10ee5360e76151c6359330113f57f5454ff73d9
C1327982 Filipe Da Silva Oliveira <c1327982@interno.bb.com.br>
2023-07-31 21:04:47 -0300

    123

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ app.get('/', (request, response)=>{
     return response.json({message: 'Server is up'});
 })
 
+app.post('/teste', (request, response)=>{
+    const {name, date} = request.body;
+
+    return response.json({name, date});
+})
+
+
 app.listen(3000);
